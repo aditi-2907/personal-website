21cc2d107e8878de54777fa64418eb562e11a6f2
unknown <tripathiaditi29s@gmail.com>
2024-08-31 01:23:07 +0530

    backend almost done

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ import bodyParser from "body-parser";
 import 'dotenv/config';
 
 const port=3000;
+var count=-1;
+var score=0;
+
+var questions= [
+  {q: "What was Aditi's age when she met Aayush?", o1: "19", o2:"18", o3:"17", o4:"16"},
+  {q: "When was our first trip?", o1: "2023", o2:"2022", o3:"2021", o4:"2020"},
+  {q: "When is Aditi's mom's birthday?", o1: "6 Dec", o2:"29 July", o3:"14 Jan", o4:"15 Feb"},
+  {q: "Song we had sex on?", o1: "Tere bina", o2:"All I Want", o3:"Zara Zara", o4:"Thousand years"},
+  {q: "Aditi's favourite Chocolate?", o1: "Silk Bubbli", o2:"Silk Mousse", o3:"Kitkat", o4:"Silk Ganache"},
+  {q: "Aditi likes Aayush most in?", o1: "black kurta", o2:"black shirt", o3:"shirtless", o4:"pathani"},
+  {q: "What Aditi enjoys the most?", o1: "Visiting new cafes", o2:"travelling", o3:"Concert", o4:"watching Movies"},
+  {q: "Our song?", o1: "Tere Hawale", o2:"Ve Haniya", o3:"Tere bina", o4:"Uska hi banana"},
+  // {q: "", o1: "", o2:"", o3:"", o4:""},
+  // {q: "", o1: "", o2:"", o3:"", o4:""}
+]
 
 const app=express();
 app.use(express.static("public"));
@@ -30,12 +45,14 @@ const db= new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "Aayush",
-    password: "1234qwer",
+    password: "Aditi123*",
     port: 5432
 });
 db.connect();
 
 app.get("/",(req,res)=>{
+  score=0;
+  count=-1;
     if(req.isAuthenticated()){
         res.render("index.ejs", {name: req.user.name});
     }
@@ -85,7 +102,7 @@ app.post("/register", async (req,res)=>{
 app.post("/login", passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/login",
-})
+  })
 );
 passport.use(
   new Strategy(async function verify(username, password, cb) {
@@ -119,7 +136,35 @@ passport.use(
     }
   })
 );
+
+app.get("/quiz",(req,res)=>{
+  count++;
+  if(count==7){
+    res.redirect("/last",{score:score});
+  }
+  res.render("quiz.ejs",{count:count, questions:questions});
+})
+
+app.get("/last",(req,res)=>{
+  res.render("last-page.ejs");
+})
   
+app.post("/check",async (req,res)=>{
+  const selected=req.body.selectedOption;
+  const result = await db.query("SELECT * FROM answers WHERE id= $1", [count,]);
+  console.log(selected);
+  console.log(result.rows[0].ans);
+  if(result.rows.length>0){
+    if(selected == result.rows[0].ans){
+      score++;
+      res.render("result.ejs", {isCorrect:1});
+    }
+    else{
+      res.render("result.ejs", {isCorrect:0});
+    }
+  }
+})
+
 passport.serializeUser((user, cb) => {
     cb(null, user);
 });
