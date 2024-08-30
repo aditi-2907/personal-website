7f9f622e6604581c81db583491bbd715dbde9f49
YASH MAHALE <yashmahale5@gmail.com>
2024-08-30 16:05:22 +0530

    login working

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const db= new pg.Client({
     user: "postgres",
     host: "localhost",
     database: "Aayush",
-    password: "Aditi123*",
+    password: "1234qwer",
     port: 5432
 });
 db.connect();
@@ -82,57 +82,50 @@ app.post("/register", async (req,res)=>{
     }
 
 })
-
 app.post("/login", passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
+  successRedirect: "/",
+  failureRedirect: "/login",
+})
+);
+passport.use(
+  new Strategy(async function verify(username, password, cb) {
+    try {
+      const result = await db.query("SELECT * FROM users WHERE name = $1 ", [
+        username,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedHashedPassword = user.password;
+        bcrypt.compare(password, storedHashedPassword, (err, valid) => {
+          if (err) {
+            //Error with password check
+            console.error("Error comparing passwords:", err);
+            return cb(err);
+          } else {
+            if (valid) {
+              //Passed password check
+              return cb(null, user);
+            } else {
+              //Did not pass password check
+              return cb(null, false);
+            }
+          }
+        });
+      } else {
+        return cb("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
   })
 );
-
-passport.use(
-    new Strategy(async function verify(name, password, cb) {
-        console.log("not entering try");
-      try {
-        console.log(1);
-        console.log(name);
-        const result = await db.query("SELECT * FROM users WHERE name = $1 ", [
-          name,
-        ]);
-        if (result.rows.length > 0) {
-          const user = result.rows[0];
-          console.log(user.name);
-          const storedHashedPassword = user.password;
-          bcrypt.compare(password, storedHashedPassword, (err, valid) => {
-            if (err) {
-              //Error with password check
-              console.error("Error comparing passwords:", err);
-              return cb(err);
-            } else {
-              if (valid) {
-                //Passed password check
-                return cb(null, user);
-              } else {
-                //Did not pass password check
-                return cb(null, false);
-              }
-            }
-          });
-        } else {
-          return cb("User not found");
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    })
-);
-
+  
 passport.serializeUser((user, cb) => {
     cb(null, user);
 });
 passport.deserializeUser((user, cb) => {
     cb(null, user);
 });
-
 app.listen(port,()=>{
     console.log(`server running on port ${port}`);
 })
